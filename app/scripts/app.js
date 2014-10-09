8a9676ca9864474f357238b386bb379989a7ea6a
Boris Fritscher <boris@fritscher.ch>
2014-10-09 09:59:36 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sqlexplorerFrontendApp
+ * @description
+ * # sqlexplorerFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sqlexplorerFrontendApp', [
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sqlexplorerFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sqlexplorerFrontendApp
+ */
+angular.module('sqlexplorerFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
